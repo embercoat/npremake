364c2fe7e6c56371c335ddf3a04448b53589c8f7
Jesper Pro <jesper.gladh@gmail.com>
2014-01-27 16:10:03 +0100

    Ändrade från 2011 till 2014 i title.
    Signed-off-by: Jesper Pro <jesper.gladh@gmail.com>

--- application/views/main.php
+++ application/views/main.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">
     <head>
-        <title>LTU - Nolleperioden 2011</title>
+        <title>LTU - Nolleperioden 2014</title>
         <style type="text/css">
             @import url('/css/resethtml5.css');
             @import url('/css/style.css');
